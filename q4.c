6f2fba812b0bc6f734c786df9bd9fb1e1995c730
harshit25csu081-coder <harshit25csu081@ncuindia.edu>
2025-10-30 18:57:15 +0530

    Create q4.c

---
+++ q4.c
@@ -0,0 +1,39 @@
+//using a temporary variable
+#include <stdio.h>
+int main() {
+    int a, b, temp;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    temp = a;
+    a = b;
+    b = temp;
+    printf("After swapping: a = %d, b = %d\n", a, b);
+    return 0;
+}
+
+//using arithmetic operation
+#include <stdio.h>
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    printf("After swapping: a = %d, b = %d\n", a, b);
+    return 0;
+}
+//using bitwise XOR Operator
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+    printf("After swapping: a = %d, b = %d\n", a, b);
+    return 0;
+}
+
